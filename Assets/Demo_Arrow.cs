13eb27037bf3cac5710660935d1c4c8d45b7f92c
Adam Biggs <wareaglebiggs@gmail.com>
2025-06-16 21:51:51 -0500

    Testing reason for crash

--- Assets/Demo_Arrow.cs
+++ Assets/Demo_Arrow.cs
@@ -12,6 +12,8 @@ public class Demo_Arrow : MonoBehaviour
     public GameObject GridArrow;
     public int Spacing = 40;
 
+    public GameObject ObjectForDestruction;
+
     void Start()
     {
         GenerateGrid();
@@ -19,33 +21,39 @@ public class Demo_Arrow : MonoBehaviour
 
     public void GenerateGrid()
     {
+        ClearGrid();
+
         Vector3 anchorLocalPos = transform.InverseTransformPoint(GridAnchor.position);
 
         for (int x = 0; x < (int)GridDimenstions.x; x++)
         {
             for (int y = 0; y < (int)GridDimenstions.y; y++)
             {
-                //grid
+                // Instantiate grid square
                 GameObject square = Instantiate(GridSquare, transform);
                 square.SetActive(true);
                 square.transform.localPosition = anchorLocalPos + new Vector3(x * Spacing, -y * Spacing, 0);
 
-                //pick answer direction
+                // Pick direction logic
                 int randomAnswer = Random.Range(0, 4);
-
-                // create list of all possible directions
                 List<int> possibleDirections = new List<int> { 0, 1, 2, 3 };
-                possibleDirections.Remove(randomAnswer);  // remove the one we're not allowed to use
-
-                // pick one of the remaining directions
+                possibleDirections.Remove(randomAnswer);
                 int randomRotation = possibleDirections[Random.Range(0, possibleDirections.Count)];
 
-                //arrow
+                // Instantiate arrow
                 GameObject arrow = Instantiate(GridArrow, square.transform);
                 arrow.SetActive(true);
                 arrow.transform.localPosition = Vector3.zero;
                 arrow.transform.localRotation = Quaternion.Euler(0, 0, randomRotation * 90f);
             }
+        }
+    }
+
+    public void ClearGrid()
+    {
+        foreach (Transform child in ObjectForDestruction.transform)
+        {
+            Destroy(child.gameObject);
         }
     }
 
