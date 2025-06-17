863a41299a50b2f50975f18f9c2b81a036a904f1
Adam Biggs <wareaglebiggs@gmail.com>
2025-06-16 21:28:07 -0500

    workin on gamify logic

--- Assets/Demo_Arrow.cs
+++ Assets/Demo_Arrow.cs
@@ -21,18 +21,24 @@ public class Demo_Arrow : MonoBehaviour
         {
             for (int y = 0; y < (int)GridDimenstions.y; y++)
             {
-                // Create the grid square
+                //grid
                 GameObject square = Instantiate(GridSquare, transform);
                 square.SetActive(true);
                 square.transform.localPosition = anchorLocalPos + new Vector3(x * Spacing, -y * Spacing, 0);
 
-                // Create the arrow as child of the square
+                //pick answer direction
+                int randomAnswer = Random.Range(0, 4);
+
+                //arrow
                 GameObject arrow = Instantiate(GridArrow, square.transform);
                 arrow.SetActive(true);
                 arrow.transform.localPosition = Vector3.zero;
-
-                // Random rotation (0, 90, 180, 270 degrees)
-                float randomRotation = 90f * Random.Range(0, 4);
+                float randomRotation = Random.Range(0, 4);
+                if (randomRotation == randomAnswer)
+                {
+                    randomRotation = Random.Range(0, 4);
+                }
+                randomRotation = randomRotation * 90f;
                 arrow.transform.localRotation = Quaternion.Euler(0, 0, randomRotation);
             }
         }
